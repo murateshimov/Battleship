4f37915e59cb8b78f9924c836a8f688725673f8d
murateshimov <murat.eshimov13@gmail.com>
2023-12-07 00:27:56 +0600

    update main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ def create_board():
     for _ in range(7):
         board.append([' '] * 7)
     return board
+
+# Функция для вывода игрового поля на экран
+def print_board(board):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("   A B C D E F G")
+    for i, row in enumerate(board):
+        print(i + 1, end="  ")
+        for cell in row:
+            print(cell, end=" ")
+        print()
